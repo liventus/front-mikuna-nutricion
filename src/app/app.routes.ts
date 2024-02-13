b0810dd89ed36cfcd3492020f0cf68aeecf8a7bf
humberto <humberto.lizana@gmail.com>
2024-02-12 21:55:10 -0500

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { TablaPacientesComponent } from './componentes/tabla-pacientes/tabla-pacientes.component';
 import { FormPacientesComponent } from './componentes/form-pacientes/form-pacientes.component'; 
 import { HistoryComponent } from './componentes/history/history.component';
-
+import { LoginComponent } from './componentes/login/login.component'; 
 
 export const routes: Routes = [
     
     { path: 'Lista-pacientes', component: TablaPacientesComponent },
     { path: 'nuevo-paciente', component: FormPacientesComponent },
     { path: 'historias-Form', component:  HistoryComponent  }    ,
+    { path: 'login', component:  LoginComponent},
     { path: '**', pathMatch: 'full', redirectTo: 'Lista-pacientes'},
 ];  
--- src/app/headers/header/header.component.ts
+++ src/app/headers/header/header.component.ts
@@ -6,16 +6,23 @@ import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';  
+import { LoginComponent } from '../../componentes/login/login.component';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule,RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [CommonModule, RouterModule, RouterLink, RouterLinkActive, RouterOutlet, LoginComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  constructor(private router: Router) { }
+
   routes: any[] = rutasgenericas
   routesAdmin: any[] = rutas  
 
+
+  
 }
